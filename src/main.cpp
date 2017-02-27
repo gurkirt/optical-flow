be3436401d82b754c2873f5acdb1a623f110c067
Gurkirt <guru094@gmail.com>
2017-02-27 16:17:55 +0000

    minor updates on 2th feb

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include "utils.h"
 #include "opencv2/imgproc/imgproc.hpp"
 
-float computeFlowForImages(string& inputdir, string& outdir, string& format, int method, int verbosity, int device_id, int bound)
-{
 
+bool replace(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = str.find(from);
+    if(start_pos == std::string::npos)
+        return false;
+    str.replace(start_pos, from.length(), to);
+    return true;
+}
+
+float computeFlowForImages(string& inputdir, string& outdir, string& format, int method, int verbosity, int device_id, int bound,int imgscale)
+{
+	
+	
 	//	string inputdir = "/mnt/jupiter-beta/DALY/images/SsazwkVoOT4/"; 
 	std::vector <std::string> filelist = listdir(inputdir,format); // get list of image in input directory endding with '.jpg'
 	//	std::string outdir = "/mnt/jupiter-beta/DALY/motion-images/SsazwkVoOT4/";
@@ -28,7 +38,7 @@ float computeFlowForImages(string& inputdir, string& outdir, string& format, int
 	Ptr<cuda::BroxOpticalFlow> brox = cuda::BroxOpticalFlow::create(0.197f, 50.0f, 0.8f, 10, 77, 10);  // instantiate Brox optical flow object
 	Ptr<cuda::FarnebackOpticalFlow> farn = cuda::FarnebackOpticalFlow::create();  // instantiate Farneback optical flow object
 	Ptr<cuda::DensePyrLKOpticalFlow> lk = cuda::DensePyrLKOpticalFlow::create(Size(7, 7));  // instantiate LK optical flow object
-	Ptr<cuda::OpticalFlowDual_TVL1> tvl1 = cuda::OpticalFlowDual_TVL1::create();  // instantiate TVL1 optical flow object
+	Ptr<cuda::OpticalFlowDual_TVL1> tvl1 = cuda::OpticalFlowDual_TVL1::create(0.25,0.15,0.3,5,5,0.01,300,0.8,0.0);  // instantiate TVL1 optical flow object
 
 	//OpticalFlowDual_TVL1_GPU alg_tvl1;
 	//BroxOpticalFlow alg_brox(0.197f, 50.0f, 0.8f, 10, 77, 10);
@@ -39,7 +49,9 @@ float computeFlowForImages(string& inputdir, string& outdir, string& format, int
 
 		fstart = getTickCount();
 		start = getTickCount();
-		float dscale = 5; 
+		
+		bool doresize = false;
+		if (imgscale>1) doresize = true;
 		cv::Size dstSize;
 		outfilename = outdir+filelist[frame_num];
 		if (!chekcIfFileExists(outfilename))
@@ -49,32 +61,32 @@ float computeFlowForImages(string& inputdir, string& outdir, string& format, int
 			if((frame_num == 0) || (cstart==0))
 			{ 
 				prev_image = cv::imread(inputdir+filelist[frame_num], IMREAD_GRAYSCALE);
-				dstSize.height = int(prev_image.rows/dscale);
-				dstSize.width = int(prev_image.cols/dscale);
+				dstSize.height = int(prev_image.rows/imgscale);
+				dstSize.width = int(prev_image.cols/imgscale);
 				
 				printf("orignal %d %d \n",prev_image.rows,prev_image.cols);
-				cv::resize(prev_image,prev_image,dstSize);
+				if (doresize) cv::resize(prev_image,prev_image,dstSize);
 				printf("after %d %d \n",prev_image.rows,prev_image.cols);
 				next_image = cv::imread(inputdir+filelist[frame_num+1], IMREAD_GRAYSCALE);
-				cv::resize(next_image,next_image,dstSize);
+				if (doresize) cv::resize(next_image,next_image,dstSize);
 
 			}
 			else if(frame_num == nb_frames-1){
 				prev_image = cv::imread(inputdir+filelist[frame_num-1], IMREAD_GRAYSCALE);
-				dstSize.height = int(prev_image.rows/dscale);
-                                dstSize.width = int(prev_image.cols/dscale);
-                                cv::resize(prev_image,prev_image,dstSize);
+				dstSize.height = int(prev_image.rows/imgscale);
+				dstSize.width = int(prev_image.cols/imgscale);
+				cv::resize(prev_image,prev_image,dstSize);
 				next_image = cv::imread(inputdir+filelist[frame_num], IMREAD_GRAYSCALE);
-				cv::resize(next_image,next_image,dstSize);
+				if (doresize) cv::resize(next_image,next_image,dstSize);
 			}
 			else{
 				//				cout<<"we are inlast else castart = "<<cstart<<endl;
 				next_image = cv::imread(inputdir+filelist[frame_num+1], IMREAD_GRAYSCALE);
-				dstSize.height = int(next_image.rows/dscale);
-                                dstSize.width = int(next_image.cols/dscale);
-				cv::resize(next_image,next_image,dstSize);
+				dstSize.height = int(next_image.rows/imgscale);
+				dstSize.width = int(next_image.cols/imgscale);
+				if (doresize) cv::resize(next_image,next_image,dstSize);
 			}
-			printf("Next %d %d \n",next_image.rows,next_image.cols);
+			//printf("Next %d %d \n",next_image.rows,next_image.cols);
 			double timeSec = (getTickCount() - start) / getTickFrequency();
 
 			if (verbosity>2) cout << "Time to load images : " << timeSec*1000 << " ms" << endl;
@@ -102,7 +114,7 @@ float computeFlowForImages(string& inputdir, string& outdir, string& format, int
 				lk->calc(frame_0,frame_1,flow);
 				break;
 			case 3:
-				lk->calc(frame_0,frame_1,flow);
+				tvl1->calc(frame_0,frame_1,flow);
 				break;
 			}
 
@@ -154,21 +166,26 @@ int computeFlowVideos(string& inputdir, string& outdir, string& format, int meth
 
 	std::vector <std::string> dirlist = listdir(inputdir);
 	//for(int i=dirlist.size()-1;i>=0;i--)
-	for(int i=start; i<end; i++)
+	//for(int i=0;i<dirlist.size();i++)
+	//for(int i=start; i>=0; i--)
+	for(int i=start; i<dirlist.size(); i++)
 	{
-		string inputBasedir = inputdir+dirlist[i]+"/";
-		string outBasedir = outdir+dirlist[i]+"/";
+		string basevidedir = dirlist[i];
+		replace(basevidedir,"\(","\\(");
+		replace(basevidedir,"\)","\\)");
+		string inputBasedir = inputdir+basevidedir+"/";
+		string outBasedir = outdir+basevidedir+"/";
 
-		float avg_time =  computeFlowForImages(inputBasedir, outBasedir, format, method, verbosity, device_id, bound);
+		float avg_time =  computeFlowForImages(inputBasedir, outBasedir, format, method, verbosity, device_id, bound,1);
 		if(verbosity>0) cout<<"Done computting for directory number "<<i<<" with avergae time of "<<avg_time<<" seconds per image"<<endl<<endl;
 
 	}
 }
 
-int computeFlowVideosOnActions(string& inputdir, string& outdir, string& format, int method, int verbosity, int device_id, int bound, int start, int end)
+int computeFlowVideosOnActions(string& inputdir, string& outdir, string& format, int method, int verbosity, int device_id, int bound ,int imgscale)
 {
 	std::vector <std::string> actiondirlist = listdir(inputdir);
-	for(int i=actiondirlist.size()-1;i>=0;i--)
+	for(int i=0; i<actiondirlist.size();i++)
 	{
 		string actiondir = inputdir+actiondirlist[i]+"/";
 		string outactiondir = outdir+actiondirlist[i]+"/";
@@ -176,10 +193,14 @@ int computeFlowVideosOnActions(string& inputdir, string& outdir, string& format,
 		for(int i=dirlist.size()-1;i>=0;i--)
 		//for(int i=start; i<end; i++)
 		{
-			string inputBasedir = actiondir+dirlist[i]+"/";
-			string outBasedir = outactiondir+dirlist[i]+"/";
+			
+			string basevidedir = dirlist[i];
+			replace(basevidedir,"\(","\\(");
+			replace(basevidedir,"\)","\\)");
+			string inputBasedir = actiondir+basevidedir+"/";
+			string outBasedir = outactiondir+basevidedir+"/";
 	
-			float avg_time =  computeFlowForImages(inputBasedir, outBasedir, format, method, verbosity, device_id, bound);
+			float avg_time =  computeFlowForImages(inputBasedir, outBasedir, format, method, verbosity, device_id, bound,imgscale);
 			if(verbosity>0) cout<<"Done computting for directory number "<<i<<" with avergae time of "<<avg_time<<" seconds per image"<<endl<<endl;
 	
 		}
@@ -192,15 +213,16 @@ int main(int argc, char **argv)
 	{
 			"{ m   method     | 0 | specify the optical flow algorithm }"
 			"{ md  multivideos | 0 | if doing it for muliple videos multivideos }"
-			"{ act   action     | 1 | if directory structure is based on action}"
-			"{ id  inputDir   | /mnt/earth-alpha/yolo-data/images/ | path of directory which contain images of folders with images where each folder belog to one video }"
-			"{ od  outputDir  | /mnt/jupiter-beta/actnet/motion-images/ | Name of the directory where flow images will be stroed }"
-			"{ f   format     | .jpg | format of input images}"
-			"{ b   bound      | 8 | specify the maximum of optical flow}"
-			"{ d   device_id  | 0  | set gpu id}"
-			"{ v   verbosity  | 1  | set gpu id}"
-			"{ sv  start  | 0  | set gpu id}"
-			"{ ev  end  | 19994  | set gpu id}"
+			"{ act action     | 0 | if directory structure is based on action }"
+			"{ id  inputDir   | /mnt/jupiter-beta/THUMOS/UCF101-rgb-Images/ | path of directory which contain images of folders with images where each folder belog to one video }"
+			"{ od  outputDir  | /mnt/jupiter-beta/THUMOS/UCF101-motion-Images/ | Name of the directory where flow images will be stroed }"
+			"{ f   format     | .jpg | format of input images }"
+			"{ b   bound      | 8 | specify the maximum of optical flow }"
+			"{ d   device_id  | 0 | set gpu id }"
+			"{ v   verbosity  | 1 | verbosity }"
+			"{ sv  start  | 0 | start video number }"
+			"{ ev  end  | 19994 | end video number }"
+			"{ ims imgscale  | 1 | imgscale }"
 	};
 
 	CommandLineParser cmd(argc, argv, keys);
@@ -216,7 +238,9 @@ int main(int argc, char **argv)
 	int start = cmd.get<int>("start");
 	int end = cmd.get<int>("end");
 	int isact = cmd.get<int>("action");
-	if (isact>0) {computeFlowVideosOnActions(inputBasedir, outBaseDir, format, method, verbosity, device_id, bound, start, end);}
+	int imgscale = cmd.get<int>("imgscale");
+	cout<<"Done reading parameters\n";
+	if (isact>0) {computeFlowVideosOnActions(inputBasedir, outBaseDir, format, method, verbosity, device_id, bound,imgscale);}
 	else {computeFlowVideos(inputBasedir, outBaseDir, format, method, verbosity, device_id, bound, start, end);}
 
 }
